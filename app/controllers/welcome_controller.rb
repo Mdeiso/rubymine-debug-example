0805fd7c4d79b7a145cd2142d201f4177da6bf33
Trent Lloyd <lathiat@bur.st>
2014-01-21 09:28:09 +0800

    Initial import of rubymine-debug-example
    This project is designed to demonstrate that the invalid call to
    this_function_doesnt_exist will trigger a browser warning and won't
    appear inside RubyMine as a caught exception showing the line that
    execution stopped, which I'd prefer so I can immediately fix the
    issue.

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,5 @@
+class WelcomeController < ApplicationController
+  def index
+    this_function_doesnt_exist
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RubymineDebugExample::Application.config.secret_key_base = '670e458565e1670da87e638a19ded6ccfb6b61657885e6e7e3b870ad14615196a6a4140d10a57c4b22891c7d6e94b447336872e95ac910c9cf90b58e129efdcb'
